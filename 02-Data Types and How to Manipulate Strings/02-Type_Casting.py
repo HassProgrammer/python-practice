0c4beb852175ed4366baca3608dd20816b01f63b
HassProgrammer <md.ahshanulalam@gmail.com>
2024-09-09 21:22:17 +0600

    Learn About Type Casting

---
+++ 02-Data Types and How to Manipulate Strings/02-Type_Casting.py
@@ -0,0 +1,7 @@
+num_char = len(input("Enter Your Name:\t"))
+print(type(num_char))
+new_num_char = str(num_char)
+print(type(new_num_char))
+print("Your Name Has "+ new_num_char +" Characters.")
+
+print(70 + float("100.5"))
