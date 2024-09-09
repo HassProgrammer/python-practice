9155af9c880abdbcaca3a5af47730f180c9469ec
HassProgrammer <md.ahshanulalam@gmail.com>
2024-09-09 12:16:35 +0600

    Make a small project with string manipulation

---
+++ 01-Working-with-variables/project-bandNameGenerator.py
@@ -0,0 +1,5 @@
+print("Welcome to 'Band Name Generator'....\n------------------")
+city = input("Enter Your Birth City Name:\t")
+pet = input("Enter Your Pet Name:\t")
+band_name = city+" "+pet
+print("Your Band Name is: "+band_name)
