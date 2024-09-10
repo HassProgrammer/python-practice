9d20a7e10c906f654c2b912b78499601883532f5
HassProgrammer <md.ahshanulalam@gmail.com>
2024-09-10 11:10:16 +0600

    Learn about operators

---
+++ 02-Data Types and How to Manipulate Strings/04-Operators.py
@@ -0,0 +1,16 @@
+2 + 3 #Addition
+7 - 6 #Subtraction
+3 * 2 #Multiplication
+6 / 3 #Division
+2 ** 2 #Exponent or Power
+
+# PEMDAS------- Orders
+# Parentheses ()
+# Exponents **
+# Multiplication *
+# Division /
+# Addition +
+# Subtraction -
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
