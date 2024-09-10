297bc879e958490e3b7ff62320f39ed16b90c285
HassProgrammer <md.ahshanulalam@gmail.com>
2024-09-10 11:49:29 +0600

    Practice modify numbers using round() and learn about f-string

---
+++ 02-Data Types and How to Manipulate Strings/06-ModifyingNumbers.py
@@ -0,0 +1,10 @@
+number = 2.6666666666666666666 
+print(round(number))
+print(round(8 / 3))
+print(round(8 / 3, 2))
+print(8 // 3)
+
+score = 0
+height = 1.69
+isWinning = True
+print(f"Your score is {score}, Your height is {height}, your winning chanse is {isWinning}")
