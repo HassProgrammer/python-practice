7d7a5ddc64de48123b62a9c19b40407eef36ae26
HassProgrammer <md.ahshanulalam@gmail.com>
2024-09-25 11:04:37 +0600

    Learn nested list and also build a project

---
+++ 04-Randomisation and Python Lists/04-nestedList.py
@@ -0,0 +1,5 @@
+fruits=['apples','oranges','bananas','mangoes','grapes','strawberry']
+vegetables = ["Tomato", "Spinach", "Asparagus", "Lettuce", "Jalapenos"]
+
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
