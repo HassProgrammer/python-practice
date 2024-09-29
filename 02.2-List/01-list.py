75bb25dcf82b897361081535b7507c1455e5b0c1
HassProgrammer <md.ahshanulalam@gmail.com>
2024-09-29 11:52:23 +0600

    problem solving using list

--- 02.2-List/01-list.py
+++ 02.2-List/01-list.py
@@ -11,3 +11,43 @@ print(tea_varieties[:2]) #if we want first two index
 #List manipulation
 tea_varieties[3] = "Herbal" 
 print(tea_varieties)
+
+for tea in tea_varieties:
+    print(tea)
+
+if "White" in tea_varieties:
+    print("I have White Tea")
+else:
+    print("Append it Bro")
+
+tea_varieties.append("White") #Append on a array
+print(tea_varieties)
+
+tea_varieties.append("Lemon")
+print(tea_varieties)
+
+# If we want to remove the last element in the list
+tea_varieties.pop()
+print(tea_varieties)
+
+tea_varieties.append("Tulshi")#Append on a array
+print(tea_varieties)
+
+#If we want to remove any item by name
+tea_varieties.remove("Tulshi")
+print(tea_varieties)
+
+# If We Want to insert a item on my comfortable position
+tea_varieties.insert(1,"Lemon Grass")
+print(tea_varieties)
+
+# If We want to copy an array to another variable 
+tea_varieties_copy = tea_varieties.copy()
+print(tea_varieties_copy)
+
+
+squared_num = [x**2 for x in range(10)]
+print(squared_num)
+
+cube_num = [x**3 for x in range(10)]
+print(cube_num)
