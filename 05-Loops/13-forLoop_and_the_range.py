e317087c36b01184e40100dc24ceca7fc432c2f6
HassProgrammer <md.ahshanulalam@gmail.com>
2024-10-07 11:32:52 +0600

    some range() practice

---
+++ 05-Loops/13-forLoop_and_the_range.py
@@ -0,0 +1,13 @@
+# for loop with range
+for i in range(1, 11):
+    print(i)
+# for loop with traversing 
+for number in  range(1, 11, 3): #3step  traversing
+    print(number) 
+
+#Sum of number
+total_number = 0
+for number in range(1,101):
+    total_number = total_number + number
+
+print(total_number)
