63202431c16e8d2bab463a4228859e9f89dfa908
HassProgrammer <md.ahshanulalam@gmail.com>
2024-10-09 10:13:58 +0600

    Write function

---
+++ 09-functions.py/01-cac_problem01.py
@@ -0,0 +1,11 @@
+# 1. Basic Function Syntax
+# Problem: Write a function to calculate and return the square of a number.
+
+def square(number):
+    print(number ** 2)
+square(2)
+
+def square2(number):
+    return number ** 3
+result = square2(5)
+print(result)
