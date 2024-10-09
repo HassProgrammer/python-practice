9f41bf77c1e548df1ef520800832e00a78a9558c
HassProgrammer <md.ahshanulalam@gmail.com>
2024-10-09 10:24:04 +0600

    Function with Multiple Parameters and returns their sum

---
+++ 09-functions.py/02-cac-_problem02.py
@@ -0,0 +1,6 @@
+# 2.Function with Multiple Parameters
+# Problem: Create a function that takes two numbers as parameters and returns their sum.
+def sum(number1, number2):
+    return number1 + number2
+result = sum(4, 2)
+print(result)
