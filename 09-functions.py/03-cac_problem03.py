ab027525d96dbd0fa37ca4863574758b0773a5b4
HassProgrammer <md.ahshanulalam@gmail.com>
2024-10-09 10:31:23 +0600

    Polymorphism in Functions

---
+++ 09-functions.py/03-cac_problem03.py
@@ -0,0 +1,8 @@
+#3. Polymorphism in Functions
+# Problem: Write a function multiply that multiplies two numbers, but can also accept and multiply strings.
+
+def multi(numOne, numTwo):
+    return numOne * numTwo
+print(multi(5, 2))
+print(multi("Khan " , 5))
+print(multi(5, "khan "))
