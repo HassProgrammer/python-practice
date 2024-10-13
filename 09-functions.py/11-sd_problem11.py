8b3cb6e89648264e04a3b5b514d719f60f3a7194
HassProgrammer <md.ahshanulalam@gmail.com>
2024-10-13 09:51:11 +0600

    WAF to print the length of a list. ( list is the parameter)

---
+++ 09-functions.py/11-sd_problem11.py
@@ -0,0 +1,9 @@
+# WAF to print the length of a list. ( list is the parameter)
+cities = ["Khulna", "Dhaka", "Chittagong", "Rajshahi","Sylhet"]
+heroes = ["thor", "ironman", "captain america", "shaktiman"]
+def print_cities(list):
+    print(len(list))
+    
+print_cities(cities)
+print_cities(heroes)
+    
