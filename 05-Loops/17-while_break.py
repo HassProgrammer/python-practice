d7cea388a37228bff4867cf8346d27bbbebbd840
HassProgrammer <md.ahshanulalam@gmail.com>
2024-10-23 09:11:40 +0600

    solved a problem about asking input while the user type exit in the input field

---
+++ 05-Loops/17-while_break.py
@@ -0,0 +1,9 @@
+pass_field = True
+
+while pass_field == True:
+      password = input("Enter a password:\t").lower()
+      if password == "exit":
+            print("Exiting the program.")
+            exit()
+      
+    
