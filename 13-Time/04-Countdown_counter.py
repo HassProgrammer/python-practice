4f160068b9116a380ff7f8845458fed60ba9c544
HassProgrammer <md.ahshanulalam@gmail.com>
2024-10-26 10:55:27 +0600

    Update the counter and reverse counter app as a timer

--- 13-Time/04-Countdown_counter.py
+++ 13-Time/04-Countdown_counter.py
@@ -6,7 +6,10 @@ if c_or_r == 'c':
         my_time = int(input("⌚Enter the time in seconds:\t"))
         print("..........Counter is on..............")
         for count in range(1, my_time+1):
-            print(count)
+            secondes = count % 60
+            minutes = int(count / 60) % 60
+            hours = int(count / 3600)
+            print(f"{hours:02}:{minutes:02}:{secondes:02}")
             time.sleep(1)
         print("Time is up!")
 
@@ -16,6 +19,9 @@ elif c_or_r == 'r':
         my_time2 = int(input("⌚Enter the time in seconds:\t"))
 
         for count in reversed(range(1, my_time2+1)):
-            print(count)
+            secondes = count % 60
+            minutes = int(count / 60) % 60
+            hours = int(count / 3600)
+            print(f"{hours:02}:{minutes:02}:{secondes:02}")
             time.sleep(1)
         print("Time is up!")
