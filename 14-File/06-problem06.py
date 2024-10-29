4026f3b00de90a179bf0444db1e6360032e74b8f
HassProgrammer <md.ahshanulalam@gmail.com>
2024-10-29 11:09:44 +0600

    From a file containing numbers separated by comma, print the count of even numbers.

---
+++ 14-File/06-problem06.py
@@ -0,0 +1,14 @@
+# From a file containing numbers separated by comma, print the count of even numbers.
+with open("14-File/06.txt", "r") as o:
+    data = o.read()
+    print(data)
+
+    num = ""
+    for i in range(len(data)):
+        if data[i] == ",":
+            print(int(num))
+            num = ""
+        else:
+            num = num + data[i]
+        
+
