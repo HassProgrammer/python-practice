40233ec145fdd01d189b0db89744f017867063da
HassProgrammer <md.ahshanulalam@gmail.com>
2024-10-30 11:38:09 +0600

    area calculation module

---
+++ 15-module/01-module_area.py
@@ -0,0 +1,5 @@
+from area import rectangle_area
+print(rectangle_area(10, 2))
+
+from area import triangle_area
+print(triangle_area(10,2))
---
+++ 15-module/area.py
@@ -0,0 +1,7 @@
+def rectangle_area(a,b):
+    result = f"area of triangle is: {a*b}"
+    return result
+
+def triangle_area (x,y) :
+    result =  f"Area of Triangle is : {0.5*x*y}"
+    return result
