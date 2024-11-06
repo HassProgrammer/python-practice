a828e0a5d6fd316f3456c4a90bca8e6a56391311
HassProgrammer <md.ahshanulalam@gmail.com>
2024-11-06 12:15:39 +0600

    First OOP in python , create an object and class

---
+++ 17-Oop/01-Basic_Class_and_Obiect.py
@@ -0,0 +1,15 @@
+class Student:
+    name = ""
+    roll = ""
+    age = ""
+    gender = ""
+    phone = ""
+
+ahshanul = Student()
+ahshanul.name = "Md Ahshanul Alam Khan"
+ahshanul.roll = 39
+ahshanul.age = "500yr"
+ahshanul.gender = "Male"
+ahshanul.phone = "01889365698"
+
+print(f"Name: {ahshanul.name}\nRoll: {ahshanul.roll}\nAge: {ahshanul.age}\nGender: {ahshanul.gender}\nContact: {ahshanul.phone}")
