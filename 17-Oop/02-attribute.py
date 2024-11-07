86f607e5791947e779f711aa5def7488dfc84d17
HassProgrammer <md.ahshanulalam@gmail.com>
2024-11-07 13:10:16 +0600

    OOP Attribute

---
+++ 17-Oop/02-attribute.py
@@ -0,0 +1,14 @@
+class Student:
+    collage_name = "Northern"
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+        print("Adding new student.")
+
+roll1 = Student("Ahshanul", 55)
+print(roll1.name, roll1.marks)
+
+roll2 = Student("Sadman", 100)
+print(roll2.name, roll2.marks)
+
+print(Student.collage_name)
