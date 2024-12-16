47416dfeff5fe7a391b8a73f6f927dc284f0ebc8
HassProgrammer <md.ahshanulalam@gmail.com>
2024-12-16 10:12:15 +0600

    te

---
+++ 17-Oop/problem solving/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+tesla = Electric_Car("Tesla", "Model S", "85kwh")
+# print("------Electric car details___________")
+# print(f"Full name: {tesla.full_name()},\nBrand: {tesla.brand},\nModel: {tesla.model},\nBattery size: {tesla.battery_size}")
