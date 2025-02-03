e5640d952b2943fe4b87a6cdc8796d4f367aedca
HassProgrammer <md.ahshanulalam@gmail.com>
2025-02-03 11:58:58 +0600

    string manipulation with slice

---
+++ 19-String/01-slice.py
@@ -0,0 +1,5 @@
+name = "Ahshan"
+print(name)
+print(name[0:3]) # start from 0 and go all the way. till 2 (3-1)
+# print(name[a:b]) # start from a and go all the way. till 2 (b-1)
+print(name[1:4]) # start from 1 and go all the way. till 2 (4-1)
